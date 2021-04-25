b95d0927bbe27744fc6fa57369f94f8590a8d617
Jiezhi <Jiezhi.G@gmail.com>
2021-04-25 17:17:11 +0800

    Create 1TwoSum.go

---
+++ 1TwoSum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func test() {
+	fmt.Println("1")
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+
+}
